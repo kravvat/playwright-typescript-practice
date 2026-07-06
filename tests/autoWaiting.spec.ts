bc8e76978cb8fa73d58004193dab29aa1bc25674
kravvat <kacperstec3d@gmail.com>
2026-07-06 19:25:04 +0200

    Learn Alternative Waits

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test"
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://uitestingplayground.com/ajax')
+    await page.getByText('Button Triggering AJAX Request').click()
+})
+
+test.skip('Auto-waiting', async ({ page }) => {
+    const successButton = page.locator('.bg-success')
+
+    await successButton.waitFor({ state: "visible" })
+    const successButtonText = await successButton.allTextContents()
+
+    expect(successButtonText).toContain('Data loaded with AJAX get request.')
+
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', { timeout: 20000 })
+})
+
+test('Alternative waits', async ({ page }) => {
+    const successButton = page.locator('.bg-success')
+
+    // wait for element
+    await page.waitForSelector('.bg-succcess')
+
+    // wait for response
+    await page.waitForRequest('http://uitestingplayground.com/ajaxdata')
+
+    // wait for network calls to be completed (NOT RECOMMENDED)
+    await page.waitForLoadState('networkidle')
+
+    const successButtonText = await successButton.allTextContents()
+    expect(successButtonText).toContain('Data loaded with AJAX get request.')
+})
